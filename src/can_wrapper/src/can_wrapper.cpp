e219cb5a52bf72e3d7f5979871bf6976dcbeb215
Marcin Walczyk <marcinwal9@gmail.com>
2023-10-23 16:05:25 +0200

    lambada

--- src/can_wrapper/src/can_wrapper.cpp
+++ src/can_wrapper/src/can_wrapper.cpp
@@ -7,21 +7,29 @@
 #include "can_wrapper/CanSocket.hpp"
 
 // Example of Ros Callback. to be deleted
-// void transferCanCallback(const std_msgs::String::ConstPtr& msg)
-// {
-	
-// }
+
+
 
 int main(int argc, char *argv[])
 {
 	ros::init(argc, argv, "can_wrapper");
 	ros::NodeHandle n;
-	CanSocket cSocket("can0");
+
+	static CanSocket cSocket("can0");
+
 	std::string errStr = cSocket.translateInitError();
 	//ROS_DEBUG(errStr);
 
 	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("/CAN/RX/raw", 256);
-	ros::Subscriber sub = n.subscribe("/CAN/TX/raw", 256, cSocket.handleRosCallback);
+	ros::Subscriber sub = n.subscribe
+	(
+		"/CAN/TX/raw",
+		256,
+		((void(*)(const std_msgs::String::ConstPtr&))[] (const std_msgs::String::ConstPtr& msg)
+		{ 
+			cSocket.handleRosCallback(msg);
+		})
+	);
 
 	ros::Rate loop_rate(1);
 	
@@ -29,7 +37,9 @@ int main(int argc, char *argv[])
 	while (ros::ok)
 	{
 		ROS_WARN("JD");
-		chatter_pub.publish("HAH");
+		std_msgs::String yyy;
+		yyy.data = "HAH";
+		chatter_pub.publish(yyy);
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
