672bda055db8db00c7f846e6db3bf5543d086a21
Marcin Walczyk <marcinwal9@gmail.com>
2024-08-16 17:33:13 +0000

    remove redundant code function doDrivingStuff was for tests only soo yeeet it goes

--- src/can_wrapper/src/can_wrapper.cpp
+++ src/can_wrapper/src/can_wrapper.cpp
@@ -14,8 +14,6 @@
 #include <ros/service.h>
 #include <std_srvs/SetBool.h>
 
-#define MOTOR_MUL 1000
-#define MOTOR_MODE VescCan::Consts::Command::SET_RPM
 
 enum class CanNodeMode
 {
@@ -97,7 +95,6 @@ int main(int argc, char *argv[])
 			break;
 
 		case CanNodeMode::Opened:
-			doDrivingStuff(motorControl);
 			break;
 
 		case CanNodeMode::Closing:
@@ -120,54 +117,5 @@ int main(int argc, char *argv[])
 		}
 		rate.sleep();
 		ros::spinOnce();
-	}
-}
-
-void doDrivingStuff(MotorControl& mtrCtl)
-{
-	can_wrapper::Wheels vel;
-
-	bool forward = XVelAxis > 0.0f;
-	float left = (std::abs(XVelAxis) + (std::max(ZRotAxis,0.0) * -2.0)) * MOTOR_MUL;
-	float right = (std::abs(XVelAxis) + (std::min(ZRotAxis,0.0) * 2.0)) * MOTOR_MUL ;
-
-	if(!forward)
-	{
-		left = -left;
-		right = -right;
-	}
-
-	ROS_INFO("Left: %01.2f ; Right: %01.2f", left, right);
-
-	vel.header.stamp = ros::Time::now();
-
-	//0-180 rotates stepper right
-	//181-359 rotates stepper left
-
-	vel.frontLeft.commandIdAngle = 4; //nope
-	vel.frontLeft.setAngle = ZRotAxis > 0 ? 269 : (ZRotAxis < 0 ? 169 : -69);	
-	vel.frontRight.commandIdAngle = 4; //nope again
-	vel.frontRight.setAngle = ZRotAxis > 0 ? 269 : (ZRotAxis < 0 ? 169 : -69);	
-	vel.rearLeft.commandIdAngle = 4; //still nope
-	vel.rearLeft.setAngle = ZRotAxis > 0 ? 269 : (ZRotAxis < 0 ? 169 : -69);	
-	vel.rearRight.commandIdAngle = 4; //nope nope nope
-	vel.rearRight.setAngle = ZRotAxis > 0 ? 269 : (ZRotAxis < 0 ? 169 : -69);	
-
-	vel.frontLeft.commandId = (int)MOTOR_MODE;
-	vel.frontLeft.setValue = left;
-	vel.rearLeft.commandId = (int)MOTOR_MODE;
-	vel.rearLeft.setValue = left;
-
-	vel.frontRight.commandId = (int)MOTOR_MODE;
-	vel.frontRight.setValue = right;
-	vel.rearRight.commandId = (int)MOTOR_MODE;
-	vel.rearRight.setValue = right;
-			
-	diff = std::chrono::system_clock::now() - lastSendWheels;
-
-	if(diff.count() > 100000000)
-	{
-		lastSendWheels = std::chrono::system_clock::now();
-		mtrCtl.sendMotorVel(vel);
 	}
 }
