80bd741621c1f9d8c6f4efe2af4933859fe58449
Raptors Nuc <jangestor@gmail.com>
2024-09-01 18:51:45 +0000

    Fixed Can turning off

--- src/ros_can_integration/src/ros_can_integration.cpp
+++ src/ros_can_integration/src/ros_can_integration.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 	ros::init(argc, argv, "ros_can_integration");
 
-	if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Warn))
+	if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info))
 	{
 		ros::console::notifyLoggerLevelsChanged();
 	}
