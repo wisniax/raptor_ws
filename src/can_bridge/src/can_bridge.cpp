fc614ac2bffd5c7127836dbd3e683c2795aa768d
Jakub Wiśniewski <101927988+wisniax@users.noreply.github.com>
2024-11-06 15:27:27 +0100

    can_bridge feature migration completed

--- src/can_bridge/src/can_bridge.cpp
+++ src/can_bridge/src/can_bridge.cpp
@@ -7,15 +7,15 @@
 
 #include "std_srvs/srv/set_bool.hpp"
 
-// #include "can_wrapper/MotorControl.hpp"
-// #include "can_wrapper/RosCanConstants.hpp"
-// #include "can_wrapper/VescStatusHandler.hpp"
-// #include "can_wrapper/RoverControl.h"
-// #include "can_wrapper/StatusMessage.hpp"
-// #include "can_wrapper/ManipulatorControl.hpp"
-// #include "can_wrapper/ProbeStatusForwarder.hpp"
+#include "can_bridge/MotorControl.hpp"
+#include "can_bridge/RosCanConstants.hpp"
+#include "can_bridge/VescStatusHandler.hpp"
+#include "can_bridge/msg/rover_control.hpp"
+#include "can_bridge/StatusMessage.hpp"
+#include "can_bridge/ManipulatorControl.hpp"
+#include "can_bridge/ProbeStatusForwarder.hpp"
 
-// #include <std_srvs/SetBool.h>
+#include <std_srvs/srv/set_bool.hpp>
 
 enum class CanNodeMode
 {
@@ -27,11 +27,15 @@ enum class CanNodeMode
 	Faulted
 };
 
-// void doDrivingStuff(MotorControl &mtrCtl);
+void doDrivingStuff(MotorControl &mtrCtl);
 
 static std::chrono::system_clock::time_point lastSendWheels;
 static std::chrono::nanoseconds diff;
 
+// TODO: 
+// - Implement this as lifecycle node
+// - Utilize a new QoS profile for the publisher and subscriber
+// - Recreate the package to use new multi-node architecture (maybe multi-threaded?)
 int main(int argc, char *argv[])
 {
 	rclcpp::init(argc, argv);
@@ -40,83 +44,83 @@ int main(int argc, char *argv[])
 	// if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Warn))
 	// 	ros::console::notifyLoggerLevelsChanged();
 
-	// MotorControl motorControl(n);
-	// VescStatusHandler mVescStatusHandler(n);
-	// StatusMessage mStatusMessage(n, true);
-	// ManipulatorControl mManipulatorCtl(n);
-	// ProbeStatusForwarder mProbeStatusForwarder(n);
+	MotorControl motorControl(n);
+	VescStatusHandler mVescStatusHandler(n);
+	StatusMessage mStatusMessage(n, true);
+	ManipulatorControl mManipulatorCtl(n);
+	ProbeStatusForwarder mProbeStatusForwarder(n);
 
 	CanNodeMode canNodeMode = CanNodeMode::Created;
 	rclcpp::Rate rate(100);
 
-	// std_srvs::SetBool::Request req;
-	// std_srvs::SetBool::Response res;
+	std_srvs::srv::SetBool::Request req;
+	std_srvs::srv::SetBool::Response res;
 
 	int iter = 0;
 
 	while (rclcpp::ok())
 	{
-		// can_wrapper::Wheels vel;
-		// switch (canNodeMode)
-		// {
-		// case CanNodeMode::Created:
+		can_bridge::msg::Wheels vel;
+		switch (canNodeMode)
+		{
+		case CanNodeMode::Created:
+			// if (!ros::service::waitForService("/CAN/ros_can_integration/check_status", 15))
+			// {
+			// 	ROS_WARN("Service /CAN/ros_can_integration/check_status is not yet available. Retrying...");
+			// 	canNodeMode = CanNodeMode::Faulted;
+			// 	break;
+			// }
 
-		// 	if (!ros::service::waitForService("/CAN/ros_can_integration/check_status", 15))
-		// 	{
-		// 		ROS_WARN("Service /CAN/ros_can_integration/check_status is not yet available. Retrying...");
-		// 		canNodeMode = CanNodeMode::Faulted;
-		// 		break;
-		// 	}
+			// if (!ros::service::call("/CAN/ros_can_integration/check_status", req, res))
+			// {
+			// 	ROS_WARN("Service /CAN/ros_can_integration/check_status call failed. Retrying...");
+			// 	canNodeMode = CanNodeMode::Faulted;
+			// 	break;
+			// }
 
-		// 	if (!ros::service::call("/CAN/ros_can_integration/check_status", req, res))
-		// 	{
-		// 		ROS_WARN("Service /CAN/ros_can_integration/check_status call failed. Retrying...");
-		// 		canNodeMode = CanNodeMode::Faulted;
-		// 		break;
-		// 	}
+			// if (res.success != 0)
+			// {
+			// 	ROS_WARN("Package ros_can_integration is not yet ready... Retrying...");
+			// 	canNodeMode = CanNodeMode::Faulted;
+			// 	break;
+			// }
+			
+			canNodeMode = CanNodeMode::Opening;
+			break;
 
-		// 	if (res.success != 0)
-		// 	{
-		// 		ROS_WARN("Package ros_can_integration is not yet ready... Retrying...");
-		// 		canNodeMode = CanNodeMode::Faulted;
-		// 		break;
-		// 	}
-		// 	canNodeMode = CanNodeMode::Opening;
-		// 	break;
+		case CanNodeMode::Opening:
+			rclcpp::sleep_for(std::chrono::milliseconds(100));
 
-		// case CanNodeMode::Opening:
-		// 	ros::Duration(0.1).sleep();
+			canNodeMode = CanNodeMode::Opened;
 
-		// 	canNodeMode = CanNodeMode::Opened;
+			break;
 
-		// 	break;
+		case CanNodeMode::Opened:
+			if ((iter++ % 100) == 9)
+			{
+				mStatusMessage.sendStatusMessage();
+			}
 
-		// case CanNodeMode::Opened:
-		// 	if ((iter++ % 100) == 9)
-		// 	{
-		// 		mStatusMessage.sendStatusMessage();
-		// 	}
+			break;
 
-		// 	break;
+		case CanNodeMode::Closing:
+			RCLCPP_INFO(n->get_logger(), "CanNodeMode::Closing");
 
-		// case CanNodeMode::Closing:
-		// 	ROS_INFO("CanNodeMode::Closing");
+			canNodeMode = CanNodeMode::Closed;
+			break;
+		case CanNodeMode::Closed:
 
-		// 	canNodeMode = CanNodeMode::Closed;
-		// 	break;
-		// case CanNodeMode::Closed:
-
-		// 	canNodeMode = CanNodeMode::Opening;
-		// 	break;
-		// case CanNodeMode::Faulted:
-		// 	ros::Duration(5).sleep();
-		// 	canNodeMode = CanNodeMode::Created;
-		// 	break;
-		// default:
-		// 	ROS_INFO("CanNodeMode::Unknown");
-		// 	canNodeMode = CanNodeMode::Faulted;
-		// 	break;
-		// }
+			canNodeMode = CanNodeMode::Opening;
+			break;
+		case CanNodeMode::Faulted:
+			rclcpp::sleep_for(std::chrono::seconds(5));
+			canNodeMode = CanNodeMode::Created;
+			break;
+		default:
+			RCLCPP_INFO(n->get_logger(), "CanNodeMode::Unknown");
+			canNodeMode = CanNodeMode::Faulted;
+			break;
+		}
 		rclcpp::spin_some(n);
 		rate.sleep();
 	}
