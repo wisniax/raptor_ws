a99b77d9c8df4e8bc1c29b781aa41c2c3c29da5b
Jakub Wiśniewski <101927988+wisniax@users.noreply.github.com>
2025-04-15 00:10:34 +0200

    Very important update

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -2,6 +2,17 @@
 
 # This script will be called by the docker container when it starts
 # and will start the REX ROS2 stuff
+
+# Most important thing: logo ;)
+echo "--------------------------------------------------------------------"
+echo 'ooooooooo.         .o.       ooooooooo.   ooooooooooooo   .oooooo.   ooooooooo.    .oooooo..o  '
+echo '`888   `Y88.      .888.      `888   `Y88. 8`   888   `8  d8P`  `Y8b  `888   `Y88. d8P`    `Y8  '
+echo ' 888   .d88`     .8"888.      888   .d88`      888      888      888  888   .d88` Y88bo.       '
+echo ' 888ooo88P`     .8` `888.     888ooo88P`       888      888      888  888ooo88P`   `"Y8888o.   '
+echo ' 888`88b.      .88ooo8888.    888              888      888      888  888`88b.        `"Y88br  '
+echo ' 888  `88b.   .8`     `888.   888              888      `88b    d88`  888  `88b.  oo     .d8P  '
+echo 'o888o  o888o o88o     o8888o o888o            o888o      `Y8bood8P`  o888o  o888o 8""88888P`   '
+
 echo "--------------------------------------------------------------------"
 echo "Wakey wakey, eggs and bakey! Hello on rex startup!"
 echo "--------------------------------------------------------------------"
