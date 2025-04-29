38e5f10a38d95ed1c99cce11c9a918ff48143cd8
Jakub Wiśniewski <101927988+wisniax@users.noreply.github.com>
2025-04-29 22:39:57 +0200

    Update src/can_bridge/include/can_bridge/RosCanConstants.hpp
    I ate the tab cuz hungry,
    nikt nie chciał na kebsa
    
    Co-authored-by: Marcin Walczyk <marcinwal9@gmail.com>

--- src/can_bridge/include/can_bridge/RosCanConstants.hpp
+++ src/can_bridge/include/can_bridge/RosCanConstants.hpp
@@ -19,7 +19,7 @@ struct RosCanConstants
 		static const std::string can_stm_errors;		/**< Topic used for reporting STM32 errors. */
 		static const std::string can_stm_init;			/**< Topic used for initializing the STM32. */
 		static const std::string can_vesc_status;		/**< Topic with vesc motor status */
-		static const std::string can_manipulator_ctl;	/**< Topic used for manipulator 'manipulation' */
+		static const std::string can_manipulator_ctl;		/**< Topic used for manipulator 'manipulation' */
 		static const std::string can_probe_status;		/**< Topic with probe status */
 		static const std::string mqtt_rover_status;		/**< Topic for rover status from MQTT. */
 	};
