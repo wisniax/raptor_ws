5ff0708d5eb76b8938409d0564bf935a7f5b27c0
wisniax <wisnia20012@gmail.com>
2025-08-28 02:06:51 +0200

    Battery info is LIVE

--- src/can_bridge/src/BatteryInfoForwarder.cpp
+++ src/can_bridge/src/BatteryInfoForwarder.cpp
@@ -2,12 +2,12 @@
 
 BatteryInfoForwarder::BatteryInfoForwarder(rclcpp::Node::SharedPtr &nh) : mNh(nh)
 {
-    const rclcpp::QoS qos = rclcpp::QoS(rclcpp::KeepLast(256));
-    mBatteryInfoGrabber = mNh->create_subscription<can_msgs::msg::Frame>(
-        RosCanConstants::RosTopics::can_raw_RX, qos,
-        std::bind(&BatteryInfoForwarder::batteryInfoGrabber, this, std::placeholders::_1));
-    mBatteryInfoPublisher = mNh->create_publisher<rex_interfaces::msg::BatteryInfo>(
-        RosCanConstants::RosTopics::can_battery_info, qos);
+	const rclcpp::QoS qos = rclcpp::QoS(rclcpp::KeepLast(256));
+	mBatteryInfoGrabber = mNh->create_subscription<can_msgs::msg::Frame>(
+		RosCanConstants::RosTopics::can_raw_RX, qos,
+		std::bind(&BatteryInfoForwarder::batteryInfoGrabber, this, std::placeholders::_1));
+	mBatteryInfoPublisher = mNh->create_publisher<rex_interfaces::msg::BatteryInfo>(
+		RosCanConstants::RosTopics::can_battery_info, qos);
 }
 
 void BatteryInfoForwarder::batteryInfoGrabber(const can_msgs::msg::Frame::ConstSharedPtr &frame)
@@ -41,6 +41,9 @@ void BatteryInfoForwarder::batteryInfoPublisher()
 		return;
 
 	rex_interfaces::msg::BatteryInfo status;
+
+	status.id = mBatteryInfo.vescID;
+	status.slot = mBatteryInfo.vescID & 0xF;
 
 	status.voltage = mBatteryInfo.voltage;
 	status.current = mBatteryInfo.current;
