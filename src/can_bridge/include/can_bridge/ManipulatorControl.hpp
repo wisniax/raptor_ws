087b93763354f5cbc328bc9a9a18363c32554950
245905 <245905@edu.p.lodz.pl>
2026-07-20 19:34:58 +0200

    Adjusted ManipulatorControl to handle new control mode

--- src/can_bridge/include/can_bridge/ManipulatorControl.hpp
+++ src/can_bridge/include/can_bridge/ManipulatorControl.hpp
@@ -11,11 +11,15 @@
 
 #include "can_msgs/msg/frame.hpp"
 #include "rex_interfaces/msg/manipulator_control.hpp"
+#include "rex_interfaces/msg/rover_status.hpp"
 
 extern "C"
 {
 #include <libVescCan/VESC.h>
 }
+
+using ManipulatorControlMsg = rex_interfaces::msg::ManipulatorControl;
+using RoverStatusMsg = rex_interfaces::msg::RoverStatus;
 
 class ManipulatorControl : public rclcpp::Node
 {
@@ -23,12 +27,17 @@ public:
 	ManipulatorControl(const rclcpp::NodeOptions & options);
 
 private:
-	void handleManipulatorCtl(const rex_interfaces::msg::ManipulatorControl::ConstSharedPtr &manipulatorCtlMsg);
+    bool isManipulatorMode(const RoverStatusMsg::ConstSharedPtr &msg);
+    void handleRoverStatusClb(const RoverStatusMsg::ConstSharedPtr &roverStatusMsg);
+    void handleManipulatorCtl(const rex_interfaces::msg::ManipulatorControl::ConstSharedPtr &manipulatorCtlMsg);
 
 	can_msgs::msg::Frame encodeStepper(const rex_interfaces::msg::VescMotorCommand &stepper, const VESC_Id_t vescId);
 
 	rclcpp::Publisher<can_msgs::msg::Frame>::SharedPtr mRawCanPub;							 /**< ROS publisher for raw CAN messages. */
 	rclcpp::Subscription<rex_interfaces::msg::ManipulatorControl>::SharedPtr mManipulatorCtlSub; /**< ROS subscriber for motor velocity messages. */
+    rclcpp::Subscription<RoverStatusMsg>::SharedPtr mRoverStatusSub;   /**< ROS subscriber for SamplerControl messages. */
+
+    RoverStatusMsg::ConstSharedPtr mRoverStatusMsgLast;
 };
 
 #endif // ManipulatorControl_h_
--- src/can_bridge/src/ManipulatorControl.cpp
+++ src/can_bridge/src/ManipulatorControl.cpp
@@ -9,9 +9,54 @@ ManipulatorControl::ManipulatorControl(const rclcpp::NodeOptions & options) : No
     mManipulatorCtlSub = this->create_subscription<rex_interfaces::msg::ManipulatorControl>(
 		RosCanConstants::RosTopics::can_manipulator_ctl, qos, 
 		std::bind(&ManipulatorControl::handleManipulatorCtl, this, std::placeholders::_1));
+
+    mRoverStatusSub = this->create_subscription<RoverStatusMsg>(
+            RosCanConstants::RosTopics::mqtt_rover_status, qos,
+            std::bind(&ManipulatorControl::handleRoverStatusClb, this, std::placeholders::_1));
+
+    mRoverStatusMsgLast = std::make_shared<const RoverStatusMsg>();
 }
+
+bool ManipulatorControl::isManipulatorMode(const RoverStatusMsg::ConstSharedPtr &msg)
+{
+    int32_t mode = msg->control_mode;
+
+    if (msg->communication_state != RoverStatusMsg::COMMUNICATION_STATE_OPENED)
+    {
+        return false;
+    }
+
+    // NONE, ESTOP
+    if (mode == RoverStatusMsg::CONTROL_MODE_NONE ||
+        (mode & RoverStatusMsg::CONTROL_MODE_ESTOP))
+    {
+        if (mode == 0)
+            RCLCPP_ERROR_THROTTLE(this->get_logger(), *this->get_clock(), 1 * 60 * 1000, // Throttle duration (1 minute)
+                                  "CONTROL_MODE is NONE! Treating as ESTOP.");
+
+        return false;
+    }
+
+    // ROBOTIC_ARM, ROBOTIC_ARM_AUTONOMY, CONFIG
+    return mode & (RoverStatusMsg::CONTROL_MODE_ROBOTIC_ARM |
+                   RoverStatusMsg::CONTROL_MODE_ROBOTIC_ARM_AUTONOMY  |
+                   RoverStatusMsg::CONTROL_MODE_CONFIG);
+}
+
+void ManipulatorControl::handleRoverStatusClb(const RoverStatusMsg::ConstSharedPtr &roverStatusMsg)
+{
+    mRoverStatusMsgLast = roverStatusMsg;
+}
+
 void ManipulatorControl::handleManipulatorCtl(const rex_interfaces::msg::ManipulatorControl::ConstSharedPtr& manipulatorCtlMsg)
 {
+    if (!isManipulatorMode(mRoverStatusMsgLast))
+    {
+        RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 5 * 60 * 1000, // Throttle duration (5 minutes)
+                             "When non-manipulator mode is selected, incoming ManipulatorControl messages are discarded.");
+        return;
+    }
+
 	// 7 since there are 6 axes + 1 gripper
 	std::array<can_msgs::msg::Frame, 7> sendQueue;
 	auto sendQueueIter = sendQueue.begin();
